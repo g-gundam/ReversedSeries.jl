bd9f79bef32e501ef7c8639ea6deea4f07aa7981
Documenter.jl <documenter@juliadocs.github.io>
2024-12-03 02:22:54 +0000

    build based on b6d5b29

---
+++ v1.0.0-DEV/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReversedSeries","category":"page"},{"location":"#ReversedSeries","page":"Home","title":"ReversedSeries","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReversedSeries.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReversedSeries]","category":"page"}]
+}
